442ea84d2a20653bda5664d026e8d169a51bd89e
tezc <ozan.vx@gmail.com>
2018-04-07 17:18:43 +0300

    carry

--- src/tz/core/client/FutureRequest.java
+++ src/tz/core/client/FutureRequest.java
@@ -25,38 +25,65 @@ public class FutureRequest extends CompletableFuture<ByteBuffer>
         this.request = request;
     }
 
+    /**
+     * Get sequence
+     * @return Sequence of the request
+     */
     public long getSequence()
     {
         return request.getSequence();
     }
 
+    /**
+     * Get acknowledge
+     * @return Get acknowledge sent with this request
+     */
     public long getAcknowledge()
     {
         return request.getAcknowledge();
     }
 
+    /**
+     * Get request
+     * @return Request
+     */
     public ClientReq getRequest()
     {
         return request;
     }
 
+    /**
+     * Set response
+     * @param response Encoded response
+     */
     public void setResponse(ByteBuffer response)
     {
         this.response = response;
     }
 
+    /**
+     * Get response
+     * @return Encoded response
+     */
     public ByteBuffer getResponse()
     {
         return response;
     }
 
+    /**
+     * Response received, finish the future
+     * @param response Response
+     */
     public void finish(ByteBuffer response)
     {
         this.response = response;
-
         complete(response);
     }
 
+    /**
+     * Request failed
+     * @param th Any throwable to report to caller of the future
+     */
     public void finishExceptionally(Throwable th)
     {
         this.response = null;
